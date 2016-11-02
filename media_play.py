a204a64ef83eeaa6fc5006e56d2248416f4f45d9
mrvaughn <mrvaughn@users.noreply.github.com>
2016-11-01 21:18:37 -0400

    Start adding comments to the code

--- media_play.py
+++ media_play.py
@@ -12,13 +12,20 @@ curses.cbreak()
 stdscr.keypad(True)
 stdscr.nodelay(True)
 
+# get all of the video files from the Videos directory
 filelist = os.listdir(path='/home/pi/Videos/')
 filelen = len(filelist)
 
 fileindex = 0
 
 stdscr.clear()
+
+# Launch a process in the background running omxplayer media player showing the
+#     simulated jack-o-lantern glowing eyes video.
 omx = subprocess.Popen(['omxplayer', '-b', '--loop', '/home/pi/pumpkin.mov'])
+
+# start a loop waiting on keyboard input (the "q" key will quit the program)
+#     or input from the IR sensor
 key = ''
 while key != ord('q'):
     key = stdscr.getch()
