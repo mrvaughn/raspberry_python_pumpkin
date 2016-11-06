53928bd0450ca3a2e270da7b5ee9797d07cc7ea9
mrvaughn <mrvaughn@users.noreply.github.com>
2016-11-06 00:07:44 -0400

    Update media_play.py

--- media_play.py
+++ media_play.py
@@ -30,9 +30,13 @@ key = ''
 while key != ord('q'):
     key = stdscr.getch()
     if sensor.is_pressed:
+        # kill any running videos
         os.system('killall omxplayer.bin')
+        # play the thank you video
         omx = subprocess.call(['omxplayer', '-b', '/home/pi/thanks.mov'])
+        # get the next video in the directory
         vfile = '/home/pi/Videos/' + filelist[fileindex]
+        # open a process to play the video in the background
         omx = subprocess.Popen(['omxplayer', '-b', vfile])
         sleep(.5)
         fileindex += 1
@@ -41,8 +45,16 @@ while key != ord('q'):
         os.system('killall omxplayer.bin')
         break
     else:
+        # during this loop, check whether a video is still playing. If not, launch the
+        #    simulated "eyes" video (pumpkin.mov)
         stdscr.clear()
+        # get the list of process running on the Raspberry Pi
         proclist = os.popen('ps -a').read()
+        # note that when an "omxplayer" process is killed, it very often will
+        #   persist as a "<defunct>" process. So we search here for an "omxplayer"
+        #   process that is not "<defunct>" (ends with a new line character instead).
+        #   If no active process is found (one that doesn't end in "omxplayer\n", then
+        #      launch the "pumpkin.mov" video. This means that all active videos have completed.
         if proclist.find('omxplayer\n') == -1:
             os.system('killall omxplayer.bin')
             subprocess.Popen(['omxplayer', '-b', '--loop', '/home/pi/pumpkin.mov'])
